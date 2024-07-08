e767d8b8d363a83e8b4ea3b2f8f662cf0879490b
wdwdwq <qkenr7278@gmail.com>
2024-07-08 16:09:23 +0900

    list 입력시 정보가 제대로 나오도록 수정

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,7 +10,7 @@ public class App {
     //public <- 다른 클래스에서도 사용할 수 있다
     //protected <- 같은 패키지 안에서 접근 o, 상속받은 클래스에서만 접근 가능
     //private 해당클래스에서만 사용가능
-    private Scanner sc;
+    private Scanner sc; // App 안에서만 사용 가능
 
     public App(Scanner sc) { //public 모든 클래에서 쓰는 
         this.sc = sc;
@@ -23,51 +23,45 @@ public class App {
         int lastId = 0;
 
         List<Motivation> ms = new ArrayList<>();
-        // boards 라는 변수를 배열로 선언!! Board에 있는 변수만 담는다
-        while (true) {
+        while (true) { // 무한 루프
             System.out.print("command )");
             String cmd = sc.nextLine().trim();
-
             if (cmd.equals("exit")) {
                 System.out.println("==Board End==");
                 break;
-            } else if (cmd.length() == 0) {
+            } else if (cmd.isEmpty()) {
                 System.out.println("명령어 입력해");
                 continue;
             }
-
             if (cmd.equals("add")) {
-                int id = lastId + 1;
-                System.out.print("title : ");
+                int id = lastId + 1; // id 생성 후 lastId 값 증가
+                System.out.print("title : "); // 
                 String title = sc.nextLine().trim();
                 System.out.print("body : ");
                 String body = sc.nextLine().trim();
 
                 Motivation m = new Motivation(id, title, body);
-
                 ms.add(m);
-
-                Motivation motivation = new Motivation(id, title , body);
-                ms.add(motivation);
 
                 System.out.printf("%s번 게시판이 등록되었습니다\n", id);
                 lastId++;
             } else if (cmd.equals("list")) {
-                System.out.println("==Board List==");
-                System.out.printf(" id  //  title   //  body    \n");
-                System.out.println("=".repeat(35));
-                if(ms.size() == 0) {
+
+                if(ms.isEmpty()) {
                     System.out.println("등록된 글 없음");
-                }else {
-                    System.out.println("있음");
-                    System.out.println("등록된 글 갯수 : " + ms.size());
+                    continue;
+                }
+                System.out.println("== motivation list ==");
+                System.out.print("      id      //      title       \n");
+                System.out.println("=".repeat(32));
+
+                for (int i = ms.size() - 1; i >= 0; i--) {
+                    Motivation m = ms.get(i);
+                    System.out.printf("      %d      //      %s       \n",m.getId(),m.getTitle());
                 }
 
-                if(ms.size() == 0) {
+                if(ms.isEmpty()) {
                     System.out.println("등록된 게시판이 없습니다 ");
-                }else {
-                    System.out.println("있음");
-                    System.out.println("등록된 게시판 갯수 : " + ms.size());
                 }
             }
 
--- src/main/java/org/example/entity/Motivation.java
+++ src/main/java/org/example/entity/Motivation.java
@@ -5,10 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class Motivation {
     private int id;
     private String title;
     private String body;
+
+    @Override
+    public String toString() {
+        return "Motivation{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
 }
